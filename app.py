d291a59e24d8beed65fec672d80cbcfe57071a49
acheamponge <achampion.emma@gmail.com>
2020-09-07 12:57:38 -0400

    second push

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import base64
 #from wit import Wit
 
-#client = Wit('MVJCGTO5PI7CIB7WZZLTRWDIVEEDPA7D')
+client = Wit('MVJCGTO5PI7CIB7WZZLTRWDIVEEDPA7D')
 
 
 file_ = open("./1.gif", "rb")
